bbfa43d61579b10762f554e5576b5f9b2e2e0f3f
birbirsbirbirs <birbirsbirbirs@gmail.com>
2024-01-16 11:10:47 -0600

    @Table

--- src/main/java/co/pitam/aliboualidatajpa/model/Author.java
+++ src/main/java/co/pitam/aliboualidatajpa/model/Author.java
@@ -7,9 +7,12 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @Entity
+//@Table(name = "AUTHOR_TBL")
 public class Author {
     @Id
-    @GeneratedValue(
+    /*
+//    for auto generation and not table
+        @GeneratedValue(
             strategy = GenerationType.SEQUENCE,
             generator = "author_sequence"
     )
@@ -18,9 +21,48 @@ public class Author {
             sequenceName = "author_sequence",
             allocationSize = 1
     )
+     */
+
+    /*
+    //  to use table for sequence generator
+    @GeneratedValue(
+            strategy = GenerationType.TABLE,
+            generator = "author_id_gen"
+    )
+    @TableGenerator(
+            name = "author_id_gen",
+            table = "id_generator",
+            pkColumnName = "id_name",
+            valueColumnName = "id_value",
+            allocationSize = 1
+    )
+     */
+
+    @GeneratedValue
     private Integer id;
+    @Column(
+            name = "f_name",
+            length = 35
+    )
     private String firstName;
     private String lastName;
+    @Column(
+            unique = true,
+            nullable = false
+    )
     private String email;
     private int age;
+/*
+    @Column(
+            updatable = false,
+            nullable = false
+    )
+    private LocalDateTime createdAt;
+
+    @Column(
+            insertable = false
+    )
+    private LocalDateTime lastModified;
+ */
+
 }
