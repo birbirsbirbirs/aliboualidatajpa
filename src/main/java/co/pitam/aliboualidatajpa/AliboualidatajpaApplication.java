02de0a4a13d177ebd2c689b39568c9a6016a8c91
birbirsbirbirs <birbirsbirbirs@gmail.com>
2024-01-16 09:07:53 -0600

    initial commit

---
+++ src/main/java/co/pitam/aliboualidatajpa/AliboualidatajpaApplication.java
@@ -0,0 +1,13 @@
+package co.pitam.aliboualidatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliboualidatajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AliboualidatajpaApplication.class, args);
+	}
+
+}
